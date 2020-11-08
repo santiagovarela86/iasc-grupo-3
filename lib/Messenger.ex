376ef442e23610d0888a0bf2983542e9a481da33
Santiago Varela <Santiago.Varela1@gds.ey.com>
2020-11-08 13:33:22 -0300

    creating one to one chat

--- lib/Messenger.ex
+++ lib/Messenger.ex
@@ -4,4 +4,8 @@ defmodule Messenger do
     MessengerServer.create_one_to_one_chat({:global, GlobalMessenger}, oneUser, anotherUser)
   end
 
+  def get_one_to_one_chat(id_one_to_one_chat) do
+    MessengerServer.get_one_to_one_chat({:global, GlobalMessenger}, id_one_to_one_chat)
+  end
+
 end
--- lib/modules/MessengerServer.ex
+++ lib/modules/MessengerServer.ex
@@ -9,16 +9,34 @@ defmodule MessengerServer do
     GenServer.start(__MODULE__, :ok, opts)
   end
 
+  ################## OPERATIONS ##################
+
   def create_one_to_one_chat(server, oneUser, anotherUser) do
     GenServer.call server, {:create_one_to_one_chat, oneUser, anotherUser}
   end
 
+  def get_one_to_one_chat(server, id_one_to_one_chat) do
+    GenServer.call server,{:get_one_to_one_chat, id_one_to_one_chat}
+  end
+
   ################## CALLBACKS ##################
 
-  def handle_call({:create_one_to_one_chat, oneUser, anotherUser}, _from, state) do
-    # do something like this...
-    # id_chat = ChatRepository.create(oneUser, anotherUser) ### if the chat already exists... should we return the already existing id or should we crash?
-    # {:reply, id_chat, state}
+  def init(:ok) do
+    {:ok, chatRepository} = ChatRepository.start_link
+    {:ok, {chatRepository}}
+  end
+
+  def handle_call({:create_one_to_one_chat, oneUser, anotherUser}, _from, {chatRepository}) do
+    one_to_one_chat_info = %{
+      oneUser: oneUser,
+      anotherUser: anotherUser
+    }
+    id_one_to_one_chat = ChatRepository.create_one_to_one_chat(chatRepository, one_to_one_chat_info)
+    {:reply, id_one_to_one_chat, {chatRepository}}
+  end
+
+  def handle_call({:get_one_to_one_chat, id_one_to_one_chat}, _from, {chatRepository}) do
+    {:reply, ChatRepository.get_one_to_one_chat(chatRepository, id_one_to_one_chat)}
   end
 
 end
