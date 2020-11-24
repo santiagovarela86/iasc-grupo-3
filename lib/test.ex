8ca9fa7cf7581996cbf8b076f024076d287bad8a
Javier Leonardo Soto <jlsoto85@gmail.com>
2020-11-24 19:54:01 -0300

    agregado test berreta

---
+++ lib/test.ex
@@ -0,0 +1,27 @@
+defmodule Test do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  def init() do
+  end
+  def test() do
+    UsuarioServer.register_user("juan")
+    UsuarioServer.register_user("franco")
+    _pidJuan = UsuarioServer.get_user("juan")
+    _pidFranco = UsuarioServer.get_user("franco")
+    _chat_name = Usuario.iniciar_chat("juan", "franco")
+    Usuario.enviar_mensaje("juan", "franco", "holus")
+    Usuario.enviar_mensaje("franco", "juan", "hola Juan, como va?")
+    Usuario.obtener_chats("juan")
+    Usuario.obtener_chats("franco")
+    Chat.get_messages("juan", "franco")
+    UsuarioServer.register_user("juan")
+    Usuario.crear_grupo("juan", "un_grupo")
+    Usuario.enviar_mensaje_grupo("juan", "un_grupo", "hola")
+  end
+
+
+end
