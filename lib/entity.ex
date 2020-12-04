e34f67e2df070e46e876d0bb2e0433de9ca28346
Javier Leonardo Soto <jlsoto85@gmail.com>
2020-12-04 06:55:17 -0300

    implementado copiar_faltantes()

--- lib/entity.ex
+++ lib/entity.ex
@@ -2,17 +2,6 @@ defmodule Entity do
   def checksum_respuesta(agente, getter) do
     :crypto.hash(:md5, getter.(agente))
   end
-
-  def copiar_todo() do
-    #obtener todos los grupos (si no existe forma directa, se podrian registrar los nombres de grupos asociados a todos los actores)
-    #agrupar por tipo de entidad, crear las entidades con nombre = nombre_grupo, y copiar state de cada agent con copiar/2
-  end
-
-  def copiar(agente_copia, grupo_de_agentes) do
-    agente_target = primera_respuesta(grupo_de_agentes, fn(a) -> a end)
-    Agent.update(agente_copia, fn(_state) ->  Agent.get(agente_target, fn(state2) -> state2 end) end)
-  end
-
   def campo_actualizado(grupo_swarm, getter) do
     Swarm.members(grupo_swarm)
     |> Task.async_stream(fn(agente) -> Entity.checksum_respuesta(agente, getter)end)
--- lib/server_entity.ex
+++ lib/server_entity.ex
@@ -34,6 +34,61 @@ defmodule ServerEntity do
   def agregar_chat_de_grupo(chat) do
     Entity.aplicar_cambio(:server_agent, &ServerAgent.agregar_chat_de_grupo(&1, chat))
   end
+  def copiar_faltantes() do
+
+    actualizar()
+
+    {_, usuarios} = ServerEntity.get_usuarios()
+    MapSet.to_list(usuarios)
+    |> Enum.each(fn usuario ->
+        if !Enum.any?(Swarm.members(usuario),fn(pid) -> is_local(pid) end) do
+          {_, agente} = UsuarioAgent.start_link(usuario)
+          copiar(agente, {:usuario_agent, usuario})
+          Swarm.join({:usuario_agent, usuario}, agente)
+          UsuarioSupervisor.start_child(usuario)
+      end
+    end)
+
+    {_, chats} = ServerEntity.get_chats_uno_a_uno()
+    MapSet.to_list(chats)
+    |> Enum.each(fn chat ->
+        if !Enum.any?(Swarm.members(chat),fn(pid) -> is_local(pid) end) do
+          {_, agente} = ChatUnoAUnoAgent.start_link(List.first(Mapset.to_list(chat)), List.last(Mapset.to_list(chat)))
+          copiar(agente, {:chat_uno_a_uno_agent, chat})
+          Swarm.join({:chat_uno_a_uno_agent, chat}, agente)
+          ChatUnoAUnoSupervisor.start_child(chat)
+      end
+    end)
+
+    {_, chats} = ServerEntity.get_chats_seguros()
+    MapSet.to_list(chats)
+    |> Enum.each(fn chat ->
+        if !Enum.any?(Swarm.members(chat),fn(pid) -> is_local(pid) end) do
+          {_, agente} = ChatSeguroAgent.start_link(List.first(Mapset.to_list(chat)), List.last(Mapset.to_list(chat)), 0)
+          copiar(agente, {:chat_seguro_agent, chat})
+          Swarm.join({:chat_seguro_agent, chat}, agente)
+          ChatSeguroSupervisor.start_child(chat)
+      end
+    end)
+
+    {_, chats} = ServerEntity.get_chats_de_grupo()
+    MapSet.to_list(chats)
+    |> Enum.each(fn chat ->
+        if !Enum.any?(Swarm.members(chat),fn(pid) -> is_local(pid) end) do
+          {_, agente} = ChatDeGrupoAgent.start_link(List.first(Mapset.to_list(chat)), List.last(Mapset.to_list(chat)))
+          copiar(agente, {:chat_grupo_agent, chat})
+          Swarm.join({:chat_grupo_agent, chat}, agente)
+          GrupoSupervisor.start_child(chat)
+      end
+    end)
+
+  end
+
+  def copiar(agente_copia, grupo_de_agentes) do
+    {_, agente_target} = Entity.primera_respuesta(grupo_de_agentes, fn(a) -> a end)
+    Agent.update(agente_copia, fn(_state) ->  Agent.get(agente_target, fn(state2) -> state2 end) end)
+  end
+
   def actualizar_async() do
     Task.async(fn-> actualizar() end)
   end
@@ -73,4 +128,9 @@ defmodule ServerEntity do
     end
 
   end
+
+  defp is_local(pid) do
+    Enum.take(:erlang.pid_to_list(pid), 2) == '<0'
+  end
+
 end
