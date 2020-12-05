e0efe9a936d7aad8ee01f8df9ba27811ac1d313d
Franco Leto <letoefranco@gmail.com>
2020-12-05 17:31:08 -0300

    Test Start a Router

--- test/pigeon_test.exs
+++ test/pigeon_test.exs
@@ -2,4 +2,14 @@ defmodule PigeonTest do
   use ExUnit.Case
   doctest Pigeon
 
+  test "Start a Router" do
+
+      case Router.start_link([]) do
+        {:ok, pid} ->  assert(pid, "Not initialize!")
+        {:already_started, pid} -> assert(pid, "Not initialize!")
+        {:error, reason} -> IO.puts "Error: #{reason}"
+      end
+
+  end
+
 end
