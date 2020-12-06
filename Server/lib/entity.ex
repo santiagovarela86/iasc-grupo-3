8ad790e9af9a4e7e55e7d9f26a7ff7e5e8290a22
Juan Larosa <juan.larosa@despegar.com>
2020-12-05 21:35:58 -0300

    Borro viejo copiar_todo

--- Server/lib/entity.ex
+++ Server/lib/entity.ex
@@ -2,18 +2,6 @@ defmodule Entity do
   def checksum_respuesta(agente, getter) do
     :crypto.hash(:md5, inspect(getter.(agente)))
   end
-
-  def copiar_todo() do
-    IO.puts("a copiar")
-    node = Router.route(Node.self)
-    IO.puts("voy a routear a #{node}")
-    usuarios = ServerEntity.get_usuarios()
-    IO.puts("Obtuve estos usuarios: #{usuarios}")
-    Enum.map(usuarios, fn usuario -> UsuarioServer.init_user(usuario) end)
-    #obtener todos los grupos (si no existe forma directa, se podrian registrar los nombres de grupos asociados a todos los actores)
-    #agrupar por tipo de entidad, crear las entidades con nombre = nombre_grupo, y copiar state de cada agent con copiar/2
-  end
-
   def copiar(agente_copia, grupo_de_agentes) do
       case primera_respuesta(grupo_de_agentes, fn(a) -> a end) do
         nil -> {}
