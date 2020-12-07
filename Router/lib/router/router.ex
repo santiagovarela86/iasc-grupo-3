322d219f9860c3eff65bef3a967554506310d431
Juan Larosa <juan.larosa@despegar.com>
2020-12-07 02:54:57 -0300

    Agrego funcion de obtener salvo los de un nodo

--- Router/lib/router/router.ex
+++ Router/lib/router/router.ex
@@ -17,6 +17,10 @@ defmodule Router do
     GenServer.call({:global, __MODULE__}, :servers)
   end
 
+  def servers(nodo) do
+    GenServer.call({:global, __MODULE__}, {:servers, nodo})
+  end
+
   def handle_call(:route, _from, state) do
     nodo = Enum.at(get_servers(), :rand.uniform(Enum.count(get_servers())) - 1)
     IO.inspect("routeo a #{nodo}")
@@ -27,9 +31,18 @@ defmodule Router do
     {:reply, get_servers(), state}
   end
 
+  def handle_call({:servers, nodo}, _from, state) do
+    {:reply, get_servers(nodo), state}
+  end
+
   def get_servers do
     Enum.filter(Node.list(), fn nodo ->
       String.contains?(String.downcase(Atom.to_string(nodo)), "server")
     end)
   end
+
+  def get_servers(nodo_excluido) do
+    Enum.filter(get_servers(), fn node -> node != nodo_excluido end)
+  end
+
 end
